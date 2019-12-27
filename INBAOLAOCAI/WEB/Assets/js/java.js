422cfaef39f502379bfe3e860187ed048a8dda9f
lequocthailinh <lequocthailinh1@gmail.com>
2019-12-27 16:24:09 +0700

    27/12

---
+++ INBAOLAOCAI/WEB/Assets/js/java.js
@@ -0,0 +1,5 @@
+// Gọi header.html, footer.html, menu.html
+$('#header').load('load/header.html');
+$('#footer').load('load/footer.html');
+$('#trangchu').load('load/trangchu.html');
+$('#gioithieu').load('load/gthieu.html');
---
+++ INBAOLAOCAI/project file structure - tham khao/assets/js/script.js
@@ -0,0 +1,4 @@
+// Gọi header.html, footer.html, menu.html
+$('#header').load('layout/header.html');
+$('#footer').load('layout/footer.html');
+$('#menu').load('layout/menu.html');
