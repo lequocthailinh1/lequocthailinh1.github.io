74cba869fa9a3183fe9420a2db76008c2d091719
lequocthailinh <lequocthailinh1@gmail.com>
2020-01-04 18:35:50 +0700

    4/1

--- INBAOLAOCAI/WEB/Assets/js/java.js
+++ INBAOLAOCAI/WEB/Assets/js/java.js
@@ -3,3 +3,7 @@ $('#header').load('load/header.html');
 $('#footer').load('load/footer.html');
 $('#trangchu').load('load/trangchu.html');
 $('#gioithieu').load('load/gthieu.html');
+$('#doitac').load('load/doitac.html');
+$('#lienhe').load('load/lihe.html');
+$('#hoatdong').load('load/hdong.html');
+$('#sanpham').load('load/spham.html');
